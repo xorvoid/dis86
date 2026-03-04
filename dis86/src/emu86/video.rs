1f729b4b96dc57251873098f521298fb3a0e2247
xorvoid <xorvoid@gmail.com>
2026-03-04 12:12:01 -0800

    emu86: started to add video functions

---
+++ dis86/src/emu86/video.rs
@@ -0,0 +1,23 @@
+use super::machine::*;
+
+impl Machine {
+  pub fn video_interrupt_0x10(&mut self) {
+    let func = self.reg_read_u8(AH);
+    match func {
+      0x1a => self.video_get_or_set_display_combination_code(),
+      _ => panic!("unsupported video function: 0x{:x}", func),
+    }
+  }
+
+  fn video_get_or_set_display_combination_code(&mut self) {
+    let get_or_set = self.reg_read_u8(AL);
+    match get_or_set {
+      0 => { // get
+        // NOTE: JUST TO MATCH DOSBOX
+        self.reg_write_u16(AX, 0x1a);
+        self.reg_write_u16(BX, 0x8);
+      }
+      _ => panic!("unimpl"),
+    }
+  }
+}
