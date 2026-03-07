0bd22dc6452fc584e639f15653111ed535918b8f
xorvoid <xorvoid@gmail.com>
2026-03-06 19:10:21 -0800

    emu86: big refactor of the validator mirroring

--- dis86/src/emu86/validator/mirroring.rs
+++ dis86/src/emu86/validator/mirroring.rs
@@ -1,78 +1,77 @@
 use super::super::emu::Emu;
 use super::super::cpu::*;
-use crate::segoff::SegOff;
-
-struct Entry {
-  seg: u16,
-  off: u16,
-  regs: Vec<Register>,
-}
+use crate::segoff::{SegOff, Seg, Off};
 
 // FIXME: BUILD THIS UP STATICALLY, RATHER THAN ON EACH ITER
-fn get_entries() -> Vec<Entry> {
+fn build_entries(code_seg: Seg) -> Vec<Entry> {
+  let code = code_seg.unwrap_normal();
   vec![
     // Reads the timestamp, non-deterministic
-    Entry { seg: 0x0000, off: 0x010b, regs: vec![CX, DX] },
+    Entry::new_addr(code+0x0000, 0x010b, vec![CX, DX]),
 
     // overlay_0005 (HACKY, FIXME)
-    Entry { seg: 0x1d10, off: 0x1e, regs: vec![BX] },
+    Entry::new_addr(code+0x1d10, 0x001e, vec![BX]),
+
+    // Ignore result of "in al,dx", used to verify that opl hardware exists
+    // Timing can very significantly and non-deterministically
+    Entry::new_range(code+0x0bb4, 0x00ae, 0x00b3, vec![AX]),
+    Entry::new_range(code+0x0bb4, 0x00b9, 0x00db, vec![AX]),
+    Entry::new_addr( code+0x0bb4, 0x0165, vec![AX]),
+    Entry::new_addr( code+0x0bb4, 0x0168, vec![AX]),
+
+    // HACKY work around for mouse interrupt... TODO: IMPL PROPERLY
+    Entry::new_addr( code+0x0454, 0x000e, vec![AX, BX]),
+
+    // Reads current system data (non-deterministic)
+    Entry::new_addr( code+0x0000, 0x0391, vec![AX, CX, DX]),
+
+    // Reads current system time (non-deterministic)
+    Entry::new_addr( code+0x0000, 0x03a4, vec![CX, DX]),
   ]
 }
 
+#[derive(Clone, Copy)]
+enum Loc {
+  Addr(SegOff),
+  Range(Seg, Off, Off), // Inclusive
+}
+
+struct Entry {
+  loc: Loc,
+  regs: Vec<Register>,
+}
+
+impl Entry {
+  fn new_addr(seg: u16, off: u16, regs: Vec<Register>) -> Entry {
+    Entry { loc: Loc::Addr(SegOff::new(seg, off)), regs }
+  }
+  fn new_range(seg: u16, start_off: u16, end_off: u16, regs: Vec<Register>) -> Entry {
+    Entry { loc: Loc::Range(Seg::Normal(seg), Off(start_off), Off(end_off)), regs }
+  }
+}
+
+impl Loc {
+  fn matches(&self, addr: SegOff) -> bool {
+    match *self {
+      Loc::Addr(segoff) => {
+        segoff == addr
+      }
+      Loc::Range(seg, start_off, end_off) => {
+        if seg != addr.seg { return false; }
+        start_off <= addr.off && addr.off <= end_off
+      }
+    }
+  }
+}
+
 pub fn apply_overrides(addr: SegOff, emu: &mut dyn Emu, hydra_state: &Cpu) {
-  let code_seg = emu.code_load_seg().unwrap_normal();
-  for entry in &get_entries() {
-    let mirror_addr = SegOff::new(entry.seg + code_seg, entry.off);
-    if mirror_addr == addr {
+  let entries = build_entries(emu.code_load_seg());
+  for entry in &entries {
+    if entry.loc.matches(addr) {
       for reg in entry.regs.iter().cloned() {
         emu.reg_write(reg, hydra_state.reg_read(reg));
       }
       return;
     }
-  }
-
-  // // Reads the timestamp, non-deterministic
-  // if addr == SegOff::new(0x823, 0x010d) {
-  //   emu.machine.reg_write_u16(CX, hydra_state.reg_read_u16(CX));
-  //   emu.machine.reg_write_u16(DX, hydra_state.reg_read_u16(DX));
-  // }
-
-  // // overlay_0005 (HACKY, FIXME)
-  // if addr == SegOff::new(0x2533, 0x21) {
-  //   emu.reg_write_u16(BX, 0xeb); // WHY??
-  // }
-
-  // Ignore result of "in al,dx", used to verify that opl hardware exists
-  // Timing can very significantly and non-deterministically
-  if addr.seg.unwrap_normal() == 0xbb4+0x823 && 0xb9 <= addr.off.0 && addr.off.0 <= 0xdb {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-  }
-  if addr == SegOff::new(0xbb4+0x823, 0x165) {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-  }
-  if addr == SegOff::new(0xbb4+0x823, 0x168) {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-  }
-  if addr.seg.unwrap_normal() == 0xbb4+0x823 && 0xae <= addr.off.0 && addr.off.0 <= 0xb3 {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-  }
-
-  // HACKY work around for mouse interrupt... TODO: IMPL PROPERLY
-  if addr == SegOff::new(0x454+0x823, 0x000e) {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-    emu.reg_write(BX, hydra_state.reg_read(BX));
-  }
-
-  // Reads current system data (non-deterministic)
-  if addr == SegOff::new(0x000+0x823, 0x391) {
-    emu.reg_write(AX, hydra_state.reg_read(AX));
-    emu.reg_write(CX, hydra_state.reg_read(CX));
-    emu.reg_write(DX, hydra_state.reg_read(DX));
-  }
-
-  // Reads current system time (non-deterministic)
-  if addr == SegOff::new(0x000+0x823, 0x3a4) {
-    emu.reg_write(CX, hydra_state.reg_read(CX));
-    emu.reg_write(DX, hydra_state.reg_read(DX));
   }
 }
